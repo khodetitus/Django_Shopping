7e20d3560c1dc80f28ae3f52a3826ab99506f0c2
khodetitus <masoudpro2@gmail.com>
2022-12-08 21:22:48 +0330

    Maktab_78_Masoud_Zandi | products | models.py | modify file | khodetitus

--- config/config/urls.py
+++ config/config/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('customers/', include('customers.urls')),
+    path('products/', include('products.urls')),
+    path('orders/', include('orders.urls')),
 ]
