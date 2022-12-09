a885493501690bbe2457a2f3a8efdb67a71be791
khodetitus <masoudpro2@gmail.com>
2022-12-09 14:21:18 +0330

    Maktab_78_Masoud_Zandi | customers | admin.py | modify file | khodetitus

--- config/customers/admin.py
+++ config/customers/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Customer, Profile, Address, OtpCode
+from core.admin import BaseAdmin
 
 # Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Profile)
+admin.site.register(Address)
+admin.site.register(OtpCode)
