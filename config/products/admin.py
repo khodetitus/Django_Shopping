8acfa7f48ec4e365643252f8a8ca6575dc7091f5
khodetitus <masoudpro2@gmail.com>
2022-12-09 14:21:37 +0330

    Maktab_78_Masoud_Zandi | products | admin.py | modify file | khodetitus

--- config/products/admin.py
+++ config/products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category, Product, ProductFeature, Comment
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductFeature)
+admin.site.register(Comment)
