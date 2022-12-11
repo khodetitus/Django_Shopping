fc639e34983f96c20196be8746b8a1376c22d954
MasouD Zandi <84510341+khodetitus@users.noreply.github.com>
2022-12-11 22:57:10 +0330

    Delete config directory

--- config/orders/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Order, OrderItem, Coupon
-from core.admin import BaseAdmin
-
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(Coupon)
