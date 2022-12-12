77c00dc1287e32d7b30fc7c77556d290b2ee1d8e
khodetitus <masoudpro2@gmail.com>
2022-12-13 02:03:41 +0330

    Watch_Shop | products| test-urls| modify file | khodetitus

--- base/products/tests.py
+++ base/products/tests.py
@@ -19,47 +19,46 @@ class CategoryModelTest(TestCase):
 
 class ProductModelTest(TestCase):
     def setUp(self):
-        self.category = Category.objects.create(name="category", slug="category", sub_category=None, is_sub=False)
-        self.product = Product.objects.create(category=self.category, name="product", slug="product", price=1000,
-                                              available=True, stock=10, image="image", description="description")
+        self.category = Category.objects.create(name='test', slug='test', sub_category=None, is_sub=False, )
+        self.product = Product.objects.create(category=self.category, name="test", slug="test", image="test",
+                                              description="test", price=1000, is_available=True, stock=10, )
 
     def test_product_creation(self):
-        self.assertEqual(self.product.category, self.category)
-        self.assertEqual(self.product.name, "product")
-        self.assertEqual(self.product.slug, "product")
+        self.assertEqual(self.product.name, "test")
+        self.assertEqual(self.product.slug, "test")
         self.assertEqual(self.product.price, 1000)
-        self.assertEqual(self.product.available, True)
+        self.assertEqual(self.product.is_available, True)
         self.assertEqual(self.product.stock, 10)
-        self.assertEqual(self.product.image, "image")
-        self.assertEqual(self.product.description, "description")
+        self.assertEqual(self.product.image, "test")
+        self.assertEqual(self.product.description, "test")
 
     def test_product_str(self):
-        self.assertEqual(str(self.product), "product")
+        self.assertEqual(str(self.product), 'test')
 
 
 class ProductFeatureModelTest(TestCase):
     def setUp(self):
         self.category = Category.objects.create(name="category", slug="category", sub_category=None, is_sub=False)
         self.product = Product.objects.create(category=self.category, name="product", slug="product", price=1000,
-                                              available=True, stock=10, image="image", description="description")
-        self.product_feature = ProductFeature.objects.create(color="color", type="type", material="material",
+                                              is_available=True, stock=10, image="image", description="description")
+        self.product_feature = ProductFeature.objects.create(color="red", type="type", material="material",
                                                              product=self.product)
 
     def test_product_feature_creation(self):
-        self.assertEqual(self.product_feature.color, "color")
+        self.assertEqual(self.product_feature.color, "red")
         self.assertEqual(self.product_feature.type, "type")
         self.assertEqual(self.product_feature.material, "material")
         self.assertEqual(self.product_feature.product, self.product)
 
     def test_product_feature_str(self):
-        self.assertEqual(str(self.product_feature), "color")
+        self.assertEqual(str(self.product_feature), "red")
 
 
 class CommentModelTest(TestCase):
     def setUp(self):
         self.category = Category.objects.create(name="category", slug="category", sub_category=None, is_sub=False)
         self.product = Product.objects.create(category=self.category, name="product", slug="product", price=1000,
-                                              available=True, stock=10, image="image", description="description")
+                                              is_available=True, stock=10, image="image", description="description")
         self.customer = Customer.objects.create(user_name="masoud", email="masoudpro2@gmail.com",
                                                 phone_number="09120572655", password="123456")
         self.comment = Comment.objects.create(title="title", body="body", customer=None, product=self.product)
