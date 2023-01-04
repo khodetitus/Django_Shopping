101232fce392e448f8aaa21945f3152674e2f281
khodetitus <masoudpro2@gmail.com>
2023-01-04 03:32:21 +0330

    gitflow-feature-stash: API

--- base/API/serializers.py
+++ base/API/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from customers.models import Profile
+from products.models import Product, Category
 
 
+# customers app api views
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
@@ -12,3 +14,24 @@ class ProfileEditSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ('first_name', 'last_name', 'gender', 'birth_date', 'image')
+
+
+# products app api views
+class ProductListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('name', 'slug', 'price', 'category', 'image', 'description', 'is_available', 'stock')
+
+
+class CategoryListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('name', 'slug', 'sub_category', 'is_sub')
+
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('name', 'price', 'description', 'image')
+
+# orders app api views
--- base/API/views.py
+++ base/API/views.py
@@ -1,26 +1,47 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from customers.models import Profile
-from .serializers import ProfileSerializer
+from products.models import Product, Category
+from .serializers import ProfileSerializer, ProfileEditSerializer, ProductListSerializer, CategoryListSerializer, \
+    ProductDetailSerializer
 from rest_framework import status
+from rest_framework import generics
 
 
 # customers app api views
 class ProfileApiView(APIView):
     def get(self, request, user_id):
         user = Profile.objects.get(id=user_id)
-        serializer = ProfileSerializer(user)
+        serializer = ProfileSerializer(instance=user)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
-    def post(self, request, format=None):
-        return Response
 
 
 class ProfileEditApiView(APIView):
-    def get(self, request, user_id):
+    def put(self, request, user_id):
         user = Profile.objects.get(id=user_id)
-        serializer = ProfileSerializer(user)
+        serializer = ProfileEditSerializer(instance=user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# products app api views
+
+class ProductListApiView(generics.ListAPIView):
+    queryset = Product.objects.filter(is_available=True)
+    serializer_class = ProductListSerializer
+
+
+class CategoryListApiView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategoryListSerializer
+
+
+class ProductDetailApiView(APIView):
+    def get(self, request, product_id, slug_product):
+        product = Product.objects.get(id=product_id, slug=slug_product)
+        serializer = ProductDetailSerializer(instance=product)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request, format=None):
-        return Response
+# orders app api views
